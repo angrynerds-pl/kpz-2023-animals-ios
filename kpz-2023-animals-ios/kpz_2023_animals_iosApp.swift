1bb765adb9a598db3c01e0941344ef9fa01d1245
rafalkwiecien <rafalkw00@gmail.com>
2023-03-29 17:49:07 +0200

    Initial Commit

---
+++ kpz-2023-animals-ios/kpz_2023_animals_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kpz_2023_animals_iosApp.swift
+//  kpz-2023-animals-ios
+//
+//  Created by Rafał Kwiecień on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct kpz_2023_animals_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
